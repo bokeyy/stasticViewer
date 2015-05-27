dd47babfc268f6f20e9e6854a8b7ab8ab94e9186
yuanyuan <yuanyuan07@baidu.com>
2015-05-27 21:08:55 +0800

    modified:   .gitignore 	modified:   app.js 	modified:   routes/index.js 	new file:   routes/mail.js
     Untracked files:
    	public/stylesheets/style.css

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '百度统计自定义变量查看器' });
 });
 
 module.exports = router;
---
+++ routes/mail.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var MailParser = require("mailparser").MailParser;
+
+var router = express.Router();
+
+/* GET mail listing. */
+router.get('/', function (req, res, next) {
+	console.log(req);
+});
+
+module.exports = router;
