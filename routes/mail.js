3904acb866008cc7c675c599191ddf6dd304e5de
yuanyuan <yuanyuan07@baidu.com>
2015-05-28 10:51:37 +0800

    modified:   routes/mail.js

--- routes/mail.js
+++ routes/mail.js
@@ -1,24 +1,41 @@
 var express = require('express');
+var fs = require("fs");
 var MailParser = require("mailparser").MailParser;
 
 var router = express.Router();
+var parseMailData;
 
 router.post('/', function (req, res, next) {
-    req.rawBody = '';
-    req.setEncoding('utf8');
+    var data = '';
 
-    req.on('data', function(chunk) {
-        req.rawBody += chunk;
+    req.on('data', function (chunk) {
+        data += chunk;
     });
 
-    req.on('end', function() {
-        console.log(req.rawBody);
+    req.on('end', function () {
+        parseMailData(data);
         next();
     });
     res.send('ok');
 });
 
-router.get('/', function(req, res, next){
+parseMailData = function (mailData) {
+
+    var mailparser = new MailParser({
+        debug: true,
+        defaultCharset: 'utf8'
+    });
+
+    mailparser.on("headers", function (headers) {
+        console.log(headers.received);
+    });
+    mailparser.on("end", function (mail) {
+        console.log(mail); // object structure for parsed e-mail
+    });
+    mailparser.write(mailData);
+};
+
+router.get('/', function (req, res, next) {
     res.send('你好~这里木有内容')
 });
 
