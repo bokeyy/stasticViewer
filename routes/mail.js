5326e9892b39964b2b2d5c23950831c6b553bc4e
yuanyuan <yuanyuan07@baidu.com>
2015-05-28 15:33:56 +0800

    modified:   routes/mail.js

--- routes/mail.js
+++ routes/mail.js
@@ -3,43 +3,33 @@ var fs = require("fs");
 var MailParser = require("mailparser").MailParser;
 
 var router = express.Router();
-var parseMailData;
 
 router.post('/', function (req, res, next) {
-    var data = '';
+    var statMailParser = new MailParser({/*
+     debug: true,*/
+        defaultCharset: 'utf-8'
+    });
 
     req.on('data', function (chunk) {
-        data += chunk;
+        statMailParser.write(new Buffer(chunk), 'utf-8')
     });
     req.on('end', function () {/*
         fs.writeFile('~/email.eml', data, function (err) {
             if (err) throw err;
             console.log("保存文件成功");
         });*/
-        parseMailData(data);
+        statMailParser.end();
+        statMailParser.on("end", function (mail) {
+            console.log("From:", mail.from); //[{address:'sender@example.com',name:'Sender Name'}]
+            console.log("Subject:", mail.subject); // Hello world!
+            console.log("Text body:", mail.text); // How are you today?
+            mail.attachments.forEach(function(attachment){
+                console.log('附件：\n', attachment.fileName);
+            });
+        });
     });
     res.send('ok');
 });
-
-parseMailData = function (mailData) {
-
-    var mailparser = new MailParser({/*
-        debug: true,*/
-        defaultCharset: 'utf-8'
-    });
-    mailparser.on("headers", function (headers) {
-        console.log(headers.received);
-    });
-    mailparser.on("end", function (mail) {
-        console.log("From:", mail.from); //[{address:'sender@example.com',name:'Sender Name'}]
-        console.log("Subject:", mail.subject); // Hello world!
-        console.log("Text body:", mail.text); // How are you today?
-        mail.attachments.forEach(function(attachment){
-            console.log('附件：\n', attachment.fileName);
-        });
-    });
-    mailparser.write(mailData);
-};
 
 router.get('/', function (req, res, next) {
     res.send('这里木有内容')
