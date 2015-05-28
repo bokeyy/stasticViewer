0650b33c98f4d715c36104345cb46a6dd172acdc
yuanyuan <yuanyuan07@baidu.com>
2015-05-28 16:14:46 +0800

    modified:   routes/mail.js

--- routes/mail.js
+++ routes/mail.js
@@ -1,33 +1,23 @@
 var express = require('express');
-var fs = require("fs");
 var MailParser = require("mailparser").MailParser;
 
 var router = express.Router();
 
 router.post('/', function (req, res, next) {
-    var statMailParser = new MailParser({/*
-     debug: true,*/
+    var statMailParser = new MailParser({
+        /*
+         debug: true,*/
         defaultCharset: 'utf-8'
     });
-
-    req.on('data', function (chunk) {
-        statMailParser.write(new Buffer(chunk), 'utf-8')
-    });
-    req.on('end', function () {/*
-        fs.writeFile('~/email.eml', data, function (err) {
-            if (err) throw err;
-            console.log("保存文件成功");
-        });*/
-        statMailParser.end();
-        statMailParser.on("end", function (mail) {
-            console.log("From:", mail.from); //[{address:'sender@example.com',name:'Sender Name'}]
-            console.log("Subject:", mail.subject); // Hello world!
-            console.log("Text body:", mail.text); // How are you today?
-            mail.attachments.forEach(function(attachment){
-                console.log('附件：\n', attachment.fileName);
-            });
+    statMailParser.on("end", function (mail) {
+        console.log("From:", mail.from); //[{address:'sender@example.com',name:'Sender Name'}]
+        console.log("Subject:", mail.subject); // Hello world!
+        console.log("Text body:", mail.text); // How are you today?
+        mail.attachments.forEach(function (attachment) {
+            console.log('附件：\n', attachment.fileName);
         });
     });
+    res.pipe(statMailParser);
     res.send('ok');
 });
 
