66a5363426cfde746e74626895b1bded6f1931e2
yuanyuan <yuanyuan07@baidu.com>
2015-05-29 11:34:42 +0800

    modified:   package.json 	modified:   routes/mail.js

--- routes/mail.js
+++ routes/mail.js
@@ -2,9 +2,13 @@ var express = require('express');
 var fs = require('fs');
 var MailParser = require("mailparser").MailParser;
 var CSV = require("comma-separated-values");
+var AV = require('avoscloud-sdk').AV;
 
 var router = express.Router();
 var statMailParser = new MailParser();
+
+AV.initialize("06lemnqefoaigmzf7ta2n6f69jbfz37i6hpwimgymuwxk8qy", "37hhl2mxf9g0dsdvs1doy52q39cvpmsiyry7ovyap65oo10s");
+var DOMLoadedTable = AV.Object.extend("DOMLoaded");
 
 router.post('/mime', function (req, res, next) {
     statMailParser.write(req.body['body-mime']);
@@ -13,9 +17,13 @@ router.post('/mime', function (req, res, next) {
         console.log("Subject:", mail.subject); // Hello world!
         console.log("Text body:", mail.text); // How are you today?
         mail.attachments && mail.attachments.forEach(function (attachment) {
+            console.log(attachment.fileName);
+
             if (attachment.contentType === "text/csv") {
                 new CSV(attachment.content.toString()).forEach(function (array) {
-                    console.log(array);
+                    for(var name in array){
+                        console.log(name);
+                    }
                 });
             }
         });
